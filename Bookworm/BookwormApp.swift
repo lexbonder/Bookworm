174ae7f6ffd1423576d1bc8d3087cdd75dcd6a6e
Alex Bonder <lexbonder@gmail.com>
2023-08-10 07:39:59 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Alex Bonder on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
